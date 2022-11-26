5058ac0aa73cad674d4b404e022c6b45e9676341
Michael J <Michael@peoplelikesoftware.com>
2022-11-26 18:48:53 -0500

    added api functions for products

--- habit-tracker/routes/api.php
+++ habit-tracker/routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +20,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::resource('products', ProductController::class);
+Route::get('products/search/{name}', [ProductController::class, 'search']);
+//Route::get('/products', [ProductController::class, 'index']);
+//Route::get('/show-products{$id}', [ProductController::class, 'show']);
+//
+//Route::post('/create-products', [ProductController::class, 'store']);
+
+
