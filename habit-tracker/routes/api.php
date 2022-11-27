6a68942ae3803cfb0f32b238128c349ee80f4530
Michael J <Michael@peoplelikesoftware.com>
2022-11-26 21:37:46 -0500

    added login/out

--- habit-tracker/routes/api.php
+++ habit-tracker/routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\AuthController;
+
 
 
 
@@ -17,14 +19,28 @@ use App\Http\Controllers\ProductController;
 |
 */
 
+//Route::resource('products', ProductController::class);
+
+//PUBLIC ROUTES
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('products/search/{name}', [ProductController::class, 'search']);
+Route::get('/show-products/{id}', [ProductController::class, 'show']);
+Route::post('/new', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+
+//Protected Routes
+Route::group(['middleware' => ['auth:sanctum']], function(){
+    Route::post('/products', [ProductController::class, 'store']);
+    Route::put('/products/{id}', [ProductController::class, 'update']);
+    Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+
+});
+
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::resource('products', ProductController::class);
-Route::get('products/search/{name}', [ProductController::class, 'search']);
-//Route::get('/products', [ProductController::class, 'index']);
-//Route::get('/show-products{$id}', [ProductController::class, 'show']);
-//
-//Route::post('/create-products', [ProductController::class, 'store']);
-
 
